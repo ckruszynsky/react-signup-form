8fd408d8352c9b1f7c3c2cae6e82ca916324ef54
Chad Kruszynsky <ckruszynsky@gmail.com>
2018-09-08 21:59:20 -0500

    react-signup-form
    example react signup form

--- src/index.js
+++ src/index.js
@@ -1,16 +1,64 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
-import "./styles.css";
+const Input = ({ name, type, placeholder }) => (
+	<div className="input">
+		<input type={type || 'text'} />
+		<label htmlFor={name} />
+	</div>
+);
+const SignupForm = ({ className, onSubmit }) => (
+	<div className={className}>
+		<h1>Registration Form</h1>
+		<form>
+			<Input name="name" placeholder="John Doe" />
+			<Input name="username" type="email" placeholder="jdoe@somewhere.com" />
+			<Input name="password" type="password" placeholder="Password" />
+			<button type="button" onClick={onSubmit}>
+				Register <i className="fa fa-fw fa-chevron-right" />
+			</button>
+		</form>
+	</div>
+);
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			mounted: false
+		};
+	}
+
+	componentDidMount = () => {
+		this.setState({ mounted: true });
+	};
+
+	handleSubmit = e => {
+		this.setState({ mounted: false });
+		e.preventDefault();
+	};
+
+	render() {
+		var child;
+		if (this.state.mounted) {
+			child = <SignupForm className="Modal" onSubmit={this.handleSubmit} />;
+		}
+
+		return (
+			<div className="App">
+				<ReactCSSTransitionGroup
+					transitionName="example"
+					transitionEnterTimeout={500}
+					transitionLeaveTimeout={300}
+				>
+					{child}
+				</ReactCSSTransitionGroup>
+			</div>
+		);
+	}
 }
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
